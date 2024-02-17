d630c96e30eec65a2fba55bf73a51a58fada9356
isturiz <isturizmauricio@gmail.com>
2024-02-16 22:03:18 -0400

    vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://felixecl.github.io',
-  base: '/my-repo',
+  output: "server",
+  adapter: vercel()
 });
