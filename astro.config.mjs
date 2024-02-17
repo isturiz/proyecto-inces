1a830e58f73a3196dd2b47772ed46a2b365de40e
isturiz <isturizmauricio@gmail.com>
2024-02-16 22:13:32 -0400

    fix title

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
